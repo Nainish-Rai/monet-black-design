28d930f9f001df70a9e7cada1be234782d60270b
Nainish Rai <nainishraisjp@gmail.com>
2024-07-03 16:21:39 +0530

    fix: no hero bg in mobile

--- components/landing-page/HeroBackground.tsx
+++ components/landing-page/HeroBackground.tsx
@@ -1,7 +1,7 @@
 import Spline from "@splinetool/react-spline/next";
 export default function HeroBackground() {
   return (
-    <div className="w-full h-screen  bg-black absolute top-0 left-0 ">
+    <div className="w-full hidden lg:flex h-screen  bg-black absolute top-0 left-0 ">
       <Spline scene="https://prod.spline.design/s7sFUgHDaI1tU3H8/scene.splinecode" />
     </div>
   );
