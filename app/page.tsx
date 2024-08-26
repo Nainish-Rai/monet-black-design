0128d812f27b7714c91c960c2e9f1d8da334478d
arindammonet <arindam.d@monet.work>
2024-08-26 12:43:34 +0530

    fix: update meta data

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,44 @@
-"use client";
-import About from "@/components/landing-page/About";
-import { FeatureGrid } from "@/components/landing-page/FeaturesGrid";
-import Footer from "@/components/landing-page/Footer";
-import Hero from "@/components/landing-page/Hero";
-import JoinUs from "@/components/landing-page/JoinUs";
-import WhatAreWeBuilding from "@/components/landing-page/What-are-we-building";
-import MediumArticles from "@/components/Medium-Articles";
-import Navbar from "@/components/Navbar";
+import Landing from "@/components/landing-page/landing";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Monet",
+  description:
+    "Monet is a new era of loyalty programs. We're pioneering a new era of rewards using blockchain technology. Our innovative solution empowers businesses and delights customers with decentralized $POINTS - the currency of interoperable rewards.",
+  openGraph: {
+    title: "Monet",
+    description:
+      "Monet is a new era of loyalty programs. We're pioneering a new era of rewards using blockchain technology. Our innovative solution empowers businesses and delights customers with decentralized $POINTS - the currency of interoperable rewards.",
+    images: [
+      {
+        url: "https://pbs.twimg.com/profile_banners/1640700298644824068/1723583013/1500x500",
+        width: 1200,
+        height: 630,
+        alt: "Monet",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Monet",
+    description:
+      "Monet is a new era of loyalty programs. We're pioneering a new era of rewards using blockchain technology. Our innovative solution empowers businesses and delights customers with decentralized $POINTS - the currency of interoperable rewards.",
+    images: [
+      {
+        url: "https://pbs.twimg.com/profile_banners/1640700298644824068/1723583013/1500x500",
+        width: 1200,
+        height: 630,
+        alt: "Monet",
+      },
+    ],
+    creator: "@monetdotwork",
+  },
+};
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center overflow-hidden bg-black justify-between  w-full">
-      <Hero />
-      {/* <Navbar /> */}
-      {/* <FeatureGrid /> */}
-      <About />
-      <WhatAreWeBuilding />
-      <JoinUs />
-      {/* <MediumArticles /> */}
-      {/* <Footer /> */}
+      <Landing />
     </main>
   );
 }
