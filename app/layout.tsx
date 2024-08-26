c5dd89cb972391cf91c34f05ee3cd6e22c4cd8ae
arindammonet <arindam.d@monet.work>
2024-08-26 12:49:45 +0530

    fix: update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,37 @@ import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Monet Work",
-  description: "Loyalty rewards platform powered by blockchain technology",
-  keywords: ["Monet", "Blockchain", "Rewards", "Loyalty"],
+  title: "Monet",
+  description:
+    "Monet is a new era of loyalty programs. We're pioneering a new era of rewards using blockchain technology. Our innovative solution empowers businesses and delights customers with decentralized $POINTS - the currency of interoperable rewards.",
+  openGraph: {
+    title: "Monet",
+    description:
+      "Monet is a new era of loyalty programs. We're pioneering a new era of rewards using blockchain technology. Our innovative solution empowers businesses and delights customers with decentralized $POINTS - the currency of interoperable rewards.",
+    images: [
+      {
+        url: "https://pbs.twimg.com/profile_banners/1640700298644824068/1723583013/1500x500",
+        width: 1200,
+        height: 630,
+        alt: "Monet",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Monet",
+    description:
+      "Monet is a new era of loyalty programs. We're pioneering a new era of rewards using blockchain technology. Our innovative solution empowers businesses and delights customers with decentralized $POINTS - the currency of interoperable rewards.",
+    images: [
+      {
+        url: "https://pbs.twimg.com/profile_banners/1640700298644824068/1723583013/1500x500",
+        width: 1200,
+        height: 630,
+        alt: "Monet",
+      },
+    ],
+    creator: "@monetdotwork",
+  },
 };
 
 export default function RootLayout({
