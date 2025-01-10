754756b374b0d0afa4c40a664ff9955ac479036c
arindammonet <arindam.d@monet.work>
2025-01-10 11:11:43 +0530

    fix: update types

--- components/ui/button-with-icon.tsx
+++ components/ui/button-with-icon.tsx
@@ -1,60 +1,54 @@
 import { motion } from "framer-motion";
 import clsx from "clsx";
-import React, { forwardRef } from "react";
+import React, { ComponentPropsWithRef, ElementType, forwardRef } from "react";
 
-interface ButtonWithIconProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  icon: JSX.Element;
-  className?: string;
-  children?: React.ReactNode;
-  as?: React.ElementType;
+type ButtonWithIconProps<T extends ElementType> = {
+  as?: T;
   href?: string;
   openInNewTab?: boolean;
-}
+  className?: string;
+  icon?: React.ReactNode;
+} & ComponentPropsWithRef<T>;
 
-const ButtonWithIcon = forwardRef<HTMLButtonElement, ButtonWithIconProps>(
-  (
-    { icon, className, children = "Button", as, openInNewTab, href, ...props },
-    ref,
-  ) => {
-    // Use provided 'as' component, or if href is provided use 'a', otherwise use 'button'
-    const Component = as || (href ? "a" : "button");
-    const MotionComponent = motion(Component);
+const ButtonWithIcon = forwardRef<
+  HTMLElement,
+  ButtonWithIconProps<ElementType>
+>(({ as, href, openInNewTab, className, children, icon, ...props }, ref) => {
+  const Component = as || (href ? "a" : "button");
+  const MotionComponent = motion(Component as ElementType);
 
-    // Add linkProps only if href is provided or Component is 'a'
-    const linkProps = href
-      ? {
-          href,
-          target: openInNewTab ? "_blank" : undefined,
-          rel: openInNewTab ? "noopener noreferrer" : undefined,
-        }
-      : {};
+  const linkProps = href
+    ? {
+        href,
+        target: openInNewTab ? "_blank" : undefined,
+        rel: openInNewTab ? "noopener noreferrer" : undefined,
+      }
+    : {};
 
-    return (
-      <MotionComponent
-        ref={ref}
-        {...linkProps}
-        {...props}
-        whileHover={{ scale: 1.02 }}
-        whileTap={{ scale: 0.98 }}
-        transition={{ type: "spring", stiffness: 400, damping: 17 }}
-        className={clsx(
-          "group p-4 px-8 flex items-center text-black gap-2 justify-center rounded-[1rem]",
-          "bg-gradient-to-r from-[#FFEA7E] to-[#FFDB1A]",
-          className,
-        )}
+  return (
+    <MotionComponent
+      ref={ref}
+      {...linkProps}
+      {...props}
+      whileHover={{ scale: 1.02 }}
+      whileTap={{ scale: 0.98 }}
+      transition={{ type: "spring", stiffness: 400, damping: 17 }}
+      className={clsx(
+        "group p-4 px-8 flex items-center text-black gap-2 justify-center rounded-[1rem]",
+        "bg-gradient-to-r from-[#FFEA7E] to-[#FFDB1A]",
+        className,
+      )}
+    >
+      {children}
+      <motion.div
+        whileHover={{ rotate: 45 }}
+        transition={{ type: "spring", stiffness: 300, damping: 15 }}
       >
-        {children}
-        <motion.div
-          whileHover={{ rotate: 45 }}
-          transition={{ type: "spring", stiffness: 300, damping: 15 }}
-        >
-          {icon}
-        </motion.div>
-      </MotionComponent>
-    );
-  },
-);
+        {icon}
+      </motion.div>
+    </MotionComponent>
+  );
+});
 
 ButtonWithIcon.displayName = "ButtonWithIcon";
 export default ButtonWithIcon;
